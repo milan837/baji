d5b74c39c7b3df5408227b7d640847c6a7fd581f
milan837 <milanshrestha837@gmail.com>
2019-09-16 00:30:12 +0530

    transaction module

--- src/main/java/com/baji/Baji/Repository/TransactionRepository.java
+++ src/main/java/com/baji/Baji/Repository/TransactionRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 public interface TransactionRepository extends CrudRepository<Transaction,Integer> {
 
     Page<Transaction> findAllByUser(User user, Pageable pageable);
